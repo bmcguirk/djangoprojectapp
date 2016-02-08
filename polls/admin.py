a41d7c2d56bbf47b7dce98453a94628f3bda1434
bmcguirk <brian+git@morganville.co>
2016-02-08 18:19:43 -0500

    Finish tutorial, customize admin site.

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("The Question", {'fields':['question_text']}),
+        ("Date Information",{'fields':['pub_date'],
+                             'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInLine]
+    list_display = ('question_text', 'pub_date','was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ["question_text"]
+
+admin.site.site_title = "Polls Administration"
+admin.site.site_header = "Polls Administration"
+admin.site.index_title = "App Administration"
+admin.site.register(Question, QuestionAdmin)
 
 # Register your models here.
